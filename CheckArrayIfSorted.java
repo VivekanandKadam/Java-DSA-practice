fe24fb4088468c5b9757db9cc958ceeddefce918
VivekanandKadam <vivekanandkadam9518@gmail.com>
2025-11-30 16:29:29 +0530

    Program added for check array is sorted in asscending or desscending order

--- CheckArrayIfSorted.java
+++ CheckArrayIfSorted.java
@@ -1,34 +1,34 @@
 public class CheckArrayIfSorted {
     public static void main(String[] args) {
         int arr[] ={9,8,7,6,5,4,3,2};
-        boolean ascendingSorted = true;
-        boolean descendingSorted = true;
+        boolean asscendingSorted = true;
+        boolean desscendingSorted = true;
 
         System.out.println("Original array");
         for(int i = 0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
-        
-        if(ascendingSorted == true){
+
+        if(asscendingSorted == true){
             for(int i=0;i<arr.length-1;i++){
                 if(arr[i]>arr[i+1]){
-                    ascendingSorted = false;
+                    asscendingSorted = false;
                 }
                 if(arr[i]<arr[i+1]){
-                    descendingSorted = false;
+                    desscendingSorted = false;
                 }
-                if(ascendingSorted == false && descendingSorted == false){
+                if(asscendingSorted == false && desscendingSorted == false){
                     break;
                 }
                 
             }
         }
        
-        if(ascendingSorted == true){
+        if(asscendingSorted == true){
             System.out.println("\nArray is sorted in asscending order");
         }
-        else if(descendingSorted == true){
-            System.out.println("\nArray is sorted in descending order");
+        else if(desscendingSorted == true){
+            System.out.println("\nArray is sorted in desscending order");
         }
         else{
             System.out.println("\nArray is not sorted");
