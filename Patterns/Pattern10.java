952ace1c302efcf03aa274e52a37c3e113b9461b
VivekanandKadam <vivekanandkadam9518@gmail.com>
2026-01-29 01:17:05 +0530

    Tenth pattern solved

---
+++ Patterns/Pattern10.java
@@ -0,0 +1,20 @@
+public class Pattern10 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1;i <= n; i++){
+            int left = i;
+            for(int j = n;j > i;j--){
+                System.out.print("  ");
+            }
+            for(int j = 1; j < i; j++){
+                System.out.print(left+" ");
+                left--;
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print(j+" ");
+            }
+            
+            System.out.println();
+        }
+    }    
+}
