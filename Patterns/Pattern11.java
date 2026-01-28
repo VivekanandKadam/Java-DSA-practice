af92690e1ae2afd1b3557f809f7f48f121b5967c
VivekanandKadam <vivekanandkadam9518@gmail.com>
2026-01-29 01:36:22 +0530

    Leven pattern's solved

---
+++ Patterns/Pattern11.java
@@ -0,0 +1,32 @@
+public class Pattern11 {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = 1; i <=n; i++){
+            for(int j = n; j > i; j--){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for(int i = n; i > 0; i--){
+            for(int j = n; j > i; j--){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        
+    }
+}
