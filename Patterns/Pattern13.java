d970f3a7397762c67db7cd31d676b05d0e86ab18
VivekanandKadam <vivekanandkadam9518@gmail.com>
2026-01-29 20:52:40 +0530

    Pattern 13 solved

---
+++ Patterns/Pattern13.java
@@ -0,0 +1,12 @@
+public class Pattern13 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 0; i < n; i++){
+            for(int j = n;j > i; j--){
+                System.out.print("  ");
+            }
+            System.out.print("*");
+            System.out.println(" ");
+        }
+    }    
+}
