3b2947cb3cc49765bb572c9a05523a1497672a97
VivekanandKadam <vivekanandkadam9518@gmail.com>
2026-01-31 23:57:15 +0530

    Pattern 20 solved

---
+++ Patterns/Pattern20.java
@@ -0,0 +1,23 @@
+public class Pattern20 {
+    public static void main(String[] args) {
+        int n = 4;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            for(int j = n - 1; j >= i; j--){
+                System.out.print(" ");
+            }
+            for(int j = n - 1; j >= i; j--){
+                System.out.print(" ");
+            }
+            int val = i;
+            for(int j = 1; j <= i; j++){
+                
+                System.out.print(val);
+                val--;
+            }
+            System.out.println();
+        }
+    }
+}
