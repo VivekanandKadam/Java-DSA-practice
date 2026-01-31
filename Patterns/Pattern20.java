9cf606999ee837a76edd1a9f62bb23c4a57c9f3d
VivekanandKadam <vivekanandkadam9518@gmail.com>
2026-02-01 00:12:24 +0530

    Pattern 20 updated

--- Patterns/Pattern20.java
+++ Patterns/Pattern20.java
@@ -3,18 +3,18 @@ public class Pattern20 {
         int n = 4;
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= i; j++){
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             for(int j = n - 1; j >= i; j--){
-                System.out.print(" ");
+                System.out.print("  ");
             }
             for(int j = n - 1; j >= i; j--){
-                System.out.print(" ");
+                System.out.print("  ");
             }
             int val = i;
             for(int j = 1; j <= i; j++){
                 
-                System.out.print(val);
+                System.out.print(val+" ");
                 val--;
             }
             System.out.println();
