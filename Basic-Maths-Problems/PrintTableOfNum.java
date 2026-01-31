453570a88b127569961a93d5aa52746c4fbd18e0
VivekanandKadam <vivekanandkadam9518@gmail.com>
2026-01-31 15:52:27 +0530

    Program added for print table of input number

---
+++ Basic-Maths-Problems/PrintTableOfNum.java
@@ -0,0 +1,8 @@
+public class PrintTableOfNum {
+    public static void main(String[] args) {
+        int num = 2;
+        for(int i = 1; i <= 10; i++){
+            System.out.println(num +"  *  "+i+"  =  "+(num * i) );
+        }
+    }
+}
