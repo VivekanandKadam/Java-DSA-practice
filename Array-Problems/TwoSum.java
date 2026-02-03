046a50ede7d81efb37686dbf63b116fc837af31e
VivekanandKadam <vivekanandkadam9518@gmail.com>
2026-02-03 22:54:47 +0530

    Two sum problem solved

---
+++ Array-Problems/TwoSum.java
@@ -0,0 +1,25 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+      
+        int[] result = new int[2];
+        for(int i = 0;i < nums.length;i++){
+            for(int j = i + 1;j < nums.length;j++){
+                if(nums[i] + nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        for(int i = 0;i<result.length;i++){
+            System.out.print(result[i]+" ");
+        }
+    return result;   
+    }
+    public static void main(String args[]){
+        TwoSum s = new TwoSum();
+        int arr[] = {1,2,3,4,5};
+        int target = 6;
+        s.twoSum(arr,target);
+    }
+}
